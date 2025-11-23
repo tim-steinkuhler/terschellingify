57522ab3142c6dc6691fbcbdb2d5a42c062f846c
Tim Steinkühler <tim.steinkuhler@xebia.com>
2025-11-23 15:36:58 +0100

    Create UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from google import genai
 from google.genai import types
+import gradio as gr
 
 
 # Import environment variables (only relevant locally)
@@ -14,8 +15,9 @@ CONTEXT_FOLDER = "./images/context"
 client = genai.Client()
 
 
-def local_image(file_path: str) -> types.Part:
+def load_local_image(file_path: str) -> types.Part:
     """Load a local image file and return it as a types.Part object."""
+    # TODO: fail on other file types
     mime_type = (
         "image/jpeg" if file_path.lower().endswith((".jpg", ".jpeg"))
         else "image/png"
@@ -28,11 +30,6 @@ def local_image(file_path: str) -> types.Part:
         mime_type=mime_type,
     )
 
-CONTEXT_IMAGES = [
-    local_image(f"{CONTEXT_FOLDER}/{filename}")
-    for filename in os.listdir(CONTEXT_FOLDER)
-    if filename.endswith((".jpg", ".jpeg", ".png"))
-]
 
 def generate_image(prompt_text: str, input_image: types.Part, context_images: list[types.Part]) -> types.GenerateContentResponse:
     return client.models.generate_content(
@@ -40,10 +37,10 @@ def generate_image(prompt_text: str, input_image: types.Part, context_images: li
         contents=[prompt_text, input_image, *context_images],
         config=types.GenerateContentConfig(
             response_modalities=['IMAGE'],
-        #     image_config=types.ImageConfig(
-        #         aspect_ratio="1:1",
-        #         image_size="1K"
-        #     ),
+            image_config=types.ImageConfig(
+                aspect_ratio="16:9",
+                image_size="2K"
+            ),
         )
     )
 
@@ -61,15 +58,54 @@ def store_image(response: types.GenerateContentResponse, output_path: str):
             image = part.as_image()
             image.save(output_path)
 
-def main():
-    print("Loading prompt and images...")
-    prompt_text = load_prompt()
-    input_image = local_image("./images/input_image.jpg")
-    print("Generating new image...")
-    response = generate_image(prompt_text, input_image, CONTEXT_IMAGES)
+
+PROMPT_TEXT = load_prompt()
+
+CONTEXT_IMAGES = [
+    load_local_image(f"{CONTEXT_FOLDER}/{filename}")
+    for filename in os.listdir(CONTEXT_FOLDER)
+    if filename.endswith((".jpg", ".jpeg", ".png"))
+]
+
+
+def process_image(input_image_gr: gr.File) -> tuple[gr.File, gr.Image]:
+    if input_image_gr is None:
+        return "Please upload an image."
+
+    print("Loading image...")
+    input_image = load_local_image(input_image_gr.name)
+
+    print("Generating image...")
+    response = generate_image(PROMPT_TEXT, input_image, CONTEXT_IMAGES)
+
     print("Storing generated image...")
     store_image(response, "./generated_image.png")
-    print("Done. Generated image saved to './generated_image.png'.")
+    
+    return input_image_gr, gr.Image("./generated_image.png")
+
+
+# Gradio interface setup
+with gr.Blocks(fill_height=True) as app:
+    with gr.Row(height="60px"):
+        image_upload = gr.File(label="Upload image")
+        query_button = gr.Button("Terschellingify")
+
+    with gr.Row(height="fit"):
+        input = gr.Image(label="Input Image")
+        output = gr.Image(label="Terschellingified")
+
+    query_button.click(
+        process_image, 
+        inputs=[image_upload], 
+        outputs=[input, output]
+    )
+
 
 if __name__ == "__main__":
-    main()
+    app.launch()
+
+
+if __name__ == "__main__":
+    print("Loading prompt and images...")
+    input_image = load_local_image("./images/input_image.jpg")
+    app.launch()
